0884e6528fd67f5c53b9d0295664e117360c6d01
Chris Tram <chris.tram@gmail.com>
2014-10-09 00:52:27 -0700

    First commit - created method to print a plain board.

---
+++ tic_tac_toe.rb
@@ -0,0 +1,17 @@
+# Summary: create tic tac toe game. Game continues until someone wins or the board is filled.
+
+# TODO: 1 - Create method to print the board
+# TODO: 2 - Method to check whether someone has won.
+# TODO: 3 - Method to check whether anyone else can move.
+
+def print_board
+  puts
+  puts "  |  |  "
+  puts "--+--+--"
+  puts "  |  |  "
+  puts "--+--+--"
+  puts "  |  |  "
+  puts
+end
+
+print_board
